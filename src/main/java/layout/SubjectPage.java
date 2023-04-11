63845d1eb7c27b4636f41d8f77535f9db635e870
zhangchuyi <79747733@qq.com>
2023-04-11 21:43:25 +0800

    添加课程页面

---
+++ src/main/java/layout/SubjectPage.java
@@ -0,0 +1,94 @@
+package layout;
+
+import ConstantPacket.ConstantParameters;
+import entity.Subject;
+import entity.SubjectInfo;
+import util.LayoutUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+
+public class SubjectPage extends JPanel {
+    /**
+     * This Box object contains every component.
+     */
+    public Box vBox1;
+    public Box vBox2;
+    public Box vBox;
+    public Box hBox;
+    public JButton test;
+    public Subject subject = ConstantParameters.studentMap.get(2020001).getSelectedSubjects().get(1);
+    public SubjectInfo subjectInfo;
+
+    {
+        this.test = new JButton(" back ");
+        this.vBox1 = Box.createVerticalBox();
+        this.vBox2 = Box.createVerticalBox();
+        this.vBox = Box.createVerticalBox();
+        this.hBox = Box.createHorizontalBox();
+        this.subjectInfo = ConstantParameters.subjectInfoMap.get(subject.getSubjectNo());
+    }
+    public SubjectPage(){
+        System.out.println("Create SubjectPage...");
+        this.vBox.add(Box.createVerticalStrut(50));
+        JLabel subjectLabel = new JLabel(subjectInfo.getSubjectName());
+        subjectLabel.setFont(new Font(Font.SERIF,Font.BOLD,20));
+        subjectLabel.setAlignmentX(CENTER_ALIGNMENT);
+        this.vBox.add(subjectLabel);
+        this.vBox.add(Box.createVerticalStrut(30));
+
+        JLabel jID1 = new JLabel("Subject ID");
+        JLabel jGrade1 = new JLabel("Grade");
+        JLabel jCharacter1 = new JLabel("Character");
+        JLabel jCredit1 = new JLabel("Credit");
+        JLabel jStartTime1 = new JLabel("Start Time");
+        JLabel jRank1 = new JLabel("Rank");
+        this.vBox1.add(jID1);
+        this.vBox1.add(jGrade1);
+        this.vBox1.add(jCharacter1);
+        this.vBox1.add(jCredit1);
+        this.vBox1.add(jStartTime1);
+        this.vBox1.add(jRank1);
+
+        HashMap<String,String> mapSubjectInfo = LayoutUtil.getMapSubjectInfo(subject);
+        String subjectNo = mapSubjectInfo.get("ID");
+        String grade = mapSubjectInfo.get("Grade");
+        String character = mapSubjectInfo.get("Character");
+        String credit = mapSubjectInfo.get("Credit");
+        String startTime = mapSubjectInfo.get("Start Time");
+        String rank = mapSubjectInfo.get("Rank");
+
+        System.out.println(subjectNo);
+        System.out.println(grade);
+        System.out.println(character);
+        System.out.println(credit);
+        System.out.println(startTime);
+        System.out.println(rank);
+
+
+        JLabel jID2 = new JLabel(subjectNo);
+        JLabel jGrade2 = new JLabel(grade);
+        JLabel jCharacter2 = new JLabel(character);
+        JLabel jCredit2 = new JLabel(credit);
+        JLabel jStartTime2 = new JLabel(startTime);
+        JLabel jRank2 = new JLabel(rank);
+        this.vBox2.add(jID2);
+        this.vBox2.add(jGrade2);
+        this.vBox2.add(jCharacter2);
+        this.vBox2.add(jCredit2);
+        this.vBox2.add(jStartTime2);
+        this.vBox2.add(jRank2);
+
+        this.hBox.add(vBox1);
+        this.hBox.add(Box.createHorizontalStrut(20));
+        this.hBox.add(vBox2);
+        this.vBox.add(hBox);
+        this.vBox.add(Box.createVerticalStrut(30));
+        this.vBox.add(this.test);
+        this.test.setAlignmentX(CENTER_ALIGNMENT);
+        this.add(this.vBox);
+        System.out.println("Create SubjectPage : [successful]");
+    }
+
+}
